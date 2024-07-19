86639c523449d5a9c83c957ea662bba9a87746cc
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-07-19 21:28:08 +0300

    Done. M5:task13

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,7 +1,7 @@
 from django.http import Http404
 from django.shortcuts import render, redirect
 from MainApp.models import Snippet
-
+from django.core.exceptions import ObjectDoesNotExist
 
 def index_page(request):
     context = {'pagename': 'PythonBin'}
@@ -20,3 +20,14 @@ def snippets_page(request):
         'snippets': snippets,
         }
     return render(request, 'pages/view_snippets.html', context)
+
+
+def snippet_detail(request, snippet_id):
+    context = {'pagename': 'Просмотр сниппета'}
+    try:
+        snippet = Snippet.objects.get(id=snippet_id)
+    except ObjectDoesNotExist:
+        return render(request, "pages/errors.html", context | {"error": f"Snippet with id='{snippet_id} not found"})
+    else:
+        context["snippet"] = snippet
+        return render(request, "pages/snippet_detail.html", context)
