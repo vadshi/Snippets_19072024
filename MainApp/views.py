9f5a2f8065b39d5182ef92048f8ea3556e0b7ef5
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-07-19 21:29:00 +0300

    Fix view snippet_detail

--- MainApp/views.py
+++ MainApp/views.py
@@ -27,7 +27,7 @@ def snippet_detail(request, snippet_id):
     try:
         snippet = Snippet.objects.get(id=snippet_id)
     except ObjectDoesNotExist:
-        return render(request, "pages/errors.html", context | {"error": f"Snippet with id='{snippet_id} not found"})
+        return render(request, "pages/errors.html", context | {"error": f"Snippet with id={snippet_id} not found"})
     else:
         context["snippet"] = snippet
         return render(request, "pages/snippet_detail.html", context)
