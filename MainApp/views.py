85cf4813f68c516a7ffe87790150aaba09116c29
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-07-22 18:59:49 +0300

    Start django form

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,7 +1,9 @@
-from django.http import Http404
+from django.http import Http404, HttpResponse
 from django.shortcuts import render, redirect
 from MainApp.models import Snippet
 from django.core.exceptions import ObjectDoesNotExist
+from MainApp.forms import SnippetForm
+
 
 def index_page(request):
     context = {'pagename': 'PythonBin'}
@@ -9,7 +11,11 @@ def index_page(request):
 
 
 def add_snippet_page(request):
-    context = {'pagename': 'Добавление нового сниппета'}
+    form = SnippetForm()
+    context = {
+        'pagename': 'Добавление нового сниппета',
+        'form': form
+        }
     return render(request, 'pages/add_snippet.html', context)
 
 
@@ -31,3 +37,12 @@ def snippet_detail(request, snippet_id):
     else:
         context["snippet"] = snippet
         return render(request, "pages/snippet_detail.html", context)
+
+
+def create_snippet(request):
+    from pprint import pprint
+    
+    if request.method == "POST":
+        pprint(request.POST)
+        return HttpResponse("Done")
+       
--- Snippets/urls.py
+++ Snippets/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('snippets/add', views.add_snippet_page, name="snippets-add"),
     path('snippets/list', views.snippets_page, name="snippets-list"),
     path('snippets/<int:snippet_id>', views.snippet_detail, name="snippet-detail"),
+    path('snippets/create', views.create_snippet, name="create-snippet"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
