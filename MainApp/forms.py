f367ef4d3f083e708b24fbc8ef9be13cb5309dfd
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-07-22 19:56:26 +0300

    Change standart options for form

--- MainApp/forms.py
+++ MainApp/forms.py
@@ -1,12 +1,28 @@
-from django.forms import ModelForm, ValidationError
+from django.forms import ModelForm, TextInput, Textarea, ValidationError
 from MainApp.models import Snippet
 
+
+# Описание возможностей по настройке форм
+# https://docs.djangoproject.com/en/dev/ref/forms/widgets/#django.forms.Widget.attrs
 
 class SnippetForm(ModelForm):
     class Meta:
         model = Snippet
         # Описываем поля, которые будем заполнять в форме
         fields = ['name', 'lang', 'code']
+        labels = {'name': '', 'lang': "", "code": ""}
+        widgets = {
+          'name': TextInput(attrs={"class":"form-control", "style":'max-width: 300px;', 'placeholder': 'Название сниппета'}),
+          'code': Textarea(attrs={
+              'placeholder': 'Код сниппета', 
+              'rows': 5, 
+              'class': 'input-large', 
+              'style': 'width: 50% !important; resize: vertical !important;'
+              }),
+
+        }
+
+
 
     def clean_name(self):
         snippet_name = self.cleaned_data.get("name")
