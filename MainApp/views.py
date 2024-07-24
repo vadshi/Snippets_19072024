9c897d0204af1e3862857924b2854f8cb779fc45
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-07-24 19:37:53 +0300

    Done. M6:task1

--- MainApp/views.py
+++ MainApp/views.py
@@ -11,8 +11,17 @@ def index_page(request):
     context = {'pagename': 'PythonBin'}
     return render(request, 'pages/index.html', context)
 
+@login_required
+def my_snippets(request):
+    snippets = Snippet.objects.filter(user=request.user)
+    context = {
+        'pagename': 'Мои сниппетов',
+        'snippets': snippets,
+        }
+    return render(request, 'pages/view_snippets.html', context)
 
-@login_required(login_url="auth-login")
+
+@login_required(login_url="home")
 def add_snippet_page(request):
     # Создаем пустую форму при запросе GET
     if request.method == "GET":
